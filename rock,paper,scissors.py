4d56d4f8c6b6d53752086b0e7ef9ceed28608f74
Mohammad Ahmad Khan <fan.ofumar@gmail.com>
2023-12-22 21:41:42 +0530

    Update rock,paper,scissors.py
    No draw option fixed

--- rock,paper,scissors.py
+++ rock,paper,scissors.py
@@ -2,41 +2,47 @@ import random
 
 com_wins = 0
 user_wins = 0
+draws = 0
 
-options = ["rock", "paper", "scissors"]
+options = ["rock", "paper", "scissor"]
 
 while True:
-    user_input=input("plz enter rock/paper/scissors, or q to quite: ").lower()
+    user_input = input("Please enter rock/paper/scissor, or q to quit: ").lower()
 
     if user_input == "q":
         break
 
     if user_input not in options:
-        print("entred an invalid option")
+        print("Entered an invalid option")
         continue
 
     random_number = random.randint(0, 2)
     comp_pick = options[random_number]
 
-    print("computer picked ", comp_pick + ".")
+    print("Computer picked", comp_pick + ".")
 
-    if user_input == "rock" and comp_pick == "scissors":
+    if user_input == "rock" and comp_pick == "scissor":
         print("You Won !")
         user_wins +=1
 
-    elif user_input == "scissors" and comp_pick == "paper":
+    elif user_input == "scissor" and comp_pick == "paper":
         print("You Won !")
         user_wins +=1
 
-    elif user_input == "paper" and comp_pick == "rock":
+    elif user_input == "mitha" and comp_pick == "rock":
         print("You Won !")
         user_wins +=1
+
+    elif user_input == comp_pick:
+        print("draw !")
+        draws +=1
 
     else :
         print("you lost !")
         com_wins +=1
         continue
 
-print("you won : ", user_wins , "times.")
-print("computer won : ", com_wins , "times.")
-print("good bye")
+print("\nYou won:", user_wins, "times.")
+print("Computer won:", com_wins, "times.")
+print("draws : ", draws, "times.")
+print("Goodbye!")
